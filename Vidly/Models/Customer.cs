32ae4a24ee4de8cdf4c17fd3cdda53faa576d896
Ian Marshall <i>
2018-07-02 11:42:49 +0100

    Another test

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -18,7 +18,7 @@ namespace Vidly.Models
         [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
 
-        [Display(Name = "Date of Birth")] // Comment zzzzzzzzzzzzzzzzz
+        [Display(Name = "Date of Birth")] // Comment zzzzzzzzzzzzzzzzzowiejowijedoiwjdowjdowijed
         [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
     }
