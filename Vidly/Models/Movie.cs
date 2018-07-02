84f260018c36bb102825b14b22f9ae700d6fb00a
Ian Marshall <i>
2018-07-02 17:05:06 +0100

    End of section 5

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -8,22 +8,25 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "The movie name is required.")]
         [StringLength(255)]
         public string Name { get; set; }
 
         public Genre Genre { get; set; }
 
         [Display(Name = "Genre")]
-        [Required]
+        [Required(ErrorMessage =  "The genre is required.")]
         public byte GenreId { get; set; }
 
         public DateTime DateAdded { get; set; }
 
         [Display(Name = "Release Date")]
+        [Required(ErrorMessage = "The release date is required.")]
         public DateTime ReleaseDate { get; set; }
 
         [Display(Name = "Number in Stock")]
+        [Required(ErrorMessage = "The number in stock must be between 1 and 20.")]
+        [Range(1, 20)]
         public byte NumberInStock { get; set; }
     }
 }
--- Vidly/ViewModels/MovieFormViewModel.cs
+++ Vidly/ViewModels/MovieFormViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Vidly.Models;
 
 namespace Vidly.ViewModels
@@ -6,16 +8,45 @@ namespace Vidly.ViewModels
     public class MovieFormViewModel
     {
         public IEnumerable<Genre> Genres { get; set; }
-        public Movie Movie { get; set; }
+        public int? Id { get; set; }
+
+        [Required(ErrorMessage = "The movie name is required.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Display(Name = "Genre")]
+        [Required(ErrorMessage = "The genre is required.")]
+        public byte? GenreId { get; set; }
+
+        [Display(Name = "Release Date")]
+        [Required(ErrorMessage = "The release date is required.")]
+        public DateTime? ReleaseDate { get; set; }
+
+        [Display(Name = "Number in Stock")]
+        [Required(ErrorMessage = "The number in stock must be between 1 and 20.")]
+        [Range(1, 20)]
+        public byte? NumberInStock { get; set; }
+
         public string Title
         {
             get
             {
-                if (Movie != null && Movie.Id != 0)
-                    return "Edit Movie";
-
-                return "New Movie";
+                return Id != 0 ? "Edit Movie" : "New Movie";
             }
+        }
+
+        public MovieFormViewModel(Movie movie)
+        {
+            Id = movie.Id;
+            Name = movie.Name;
+            ReleaseDate = movie.ReleaseDate;
+            NumberInStock = movie.NumberInStock;
+            GenreId = movie.GenreId;
+        }
+
+        public MovieFormViewModel()
+        {
+            Id = 0;
         }
     }
 }
