68c36ce06304e9f4be8b1e816f99f418f39529d7
Ian Marshall <i>
2018-07-02 12:52:34 +0100

    Changed comment to "Comment again"

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -18,7 +18,7 @@ namespace Vidly.Models
         [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
 
-        [Display(Name = "Date of Birth")] // Comment again23
+        [Display(Name = "Date of Birth")] // Comment again
         [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
     }
