21803fdb5b94fafbf2610e9ad1e9a7b03ada9fa1
Zitman <ian@ianmarshall.me>
2018-07-05 07:58:00 +0100

    Got the creation of a new movie working via the API

--- Vidly/Dtos/MovieDto.cs
+++ Vidly/Dtos/MovieDto.cs
@@ -7,7 +7,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Dtos
 {
-    public class MovieDto
+       public class MovieDto
     {
         public int Id { get; set; }
 
@@ -15,6 +15,7 @@ namespace Vidly.Dtos
         [StringLength(255)]
         public string Name { get; set; }
 
+        [Required]
         public byte GenreId { get; set; }
 
         public DateTime DateAdded { get; set; }
@@ -24,4 +25,4 @@ namespace Vidly.Dtos
         [Range(1, 20)]
         public byte NumberInStock { get; set; }
     }
-}
+} 
