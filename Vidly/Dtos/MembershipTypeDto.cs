3acf3e187a666d98d8e1ab166bd861179bb9c86f
Ian Marshall <i>
2018-07-09 08:53:40 +0100

    After Lecture 82

--- Vidly/Dtos/MembershipTypeDto.cs
+++ Vidly/Dtos/MembershipTypeDto.cs
@@ -9,13 +9,6 @@ namespace Vidly.Dtos
     public class MembershipTypeDto
     {
         public byte Id { get; set; }
-        [Required]
         public string Name { get; set; }
-        public short SignUpFee { get; set; }
-        public byte DurationInMonths { get; set; }
-        public byte DiscountRate { get; set; }
-
-        public static readonly byte Unknown = 0;
-        public static readonly byte PayAsYouGo = 1;
     }
 }
